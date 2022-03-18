8e1d14d6d7016a3725621088cb6d96625201d290
Jose Epifanio <jose.epifanio90@gmail.com>
2022-03-18 16:41:55 -0400

    Add quick example to test out the middleware

---
+++ examples/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	fireGO "github.com/JEpifanio90/FireGO"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.New()
+	auth := r.Group("/auth")
+	auth.Use(fireGO.AuthMiddleware())
+	auth.GET("/", func(c *gin.Context) {
+		claims := fireGO.ExtractClaims(c)
+		fmt.Println(claims)
+		c.String(http.StatusOK, "success")
+	})
+	r.Run()
+}
